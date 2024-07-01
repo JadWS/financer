e785055872320b3d69b53a650013a66e8b984217
JadWS <jadwalidsamadi@gmail.com>
2024-07-01 11:12:50 +0200

    restore express

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,59 +5,30 @@ import http from 'http'
 import cors from 'cors'
 import mikroOrmConfig from './mikro-orm.config'
 
-var { ruruHTML } = require("ruru/server")
-
 export const DI = {} as {
     server: http.Server;
     orm: MikroORM<PostgreSqlDriver>,
     em: EntityManager<PostgreSqlDriver>
 };
 
-var { createHandler } = require("graphql-http/lib/use/express")
-var { buildSchema } = require("graphql")
-
-var schema = buildSchema(`
-    type Query {
-      hello: String
-    }
-  `)
-
-var root = {
-    hello() {
-        return "Hello world!"
-    },
-}
-
 var app = express()
-
 
 export const init = (async () => {
     DI.orm = await MikroORM.init<PostgreSqlDriver>(mikroOrmConfig);
     DI.em = DI.orm.em
 
     app.use(cors({
-        origin: ["http://localhost:5173"],
+        origin: ["https://lbspotlight.org", "https://www.lbspotlight.org", "http://localhost:5173"],
         credentials: true,
         optionsSuccessStatus: 200
     }))
 
     app.use(express.json());
-    app.use((_req, _res, next) => RequestContext.create(DI.em, next));
+    app.use((_req, _res, next) => RequestContext.create(DI.orm.em, next));
 
-    app.all(
-        "/graphql",
-        createHandler({
-            schema: schema,
-            rootValue: root,
-        })
-    )
-
-    app.get("/", (_req, res) => {
-        res.type("html")
-        res.end(ruruHTML({ endpoint: "/graphql" }))
-    })
+    app.get('/', (_req, res) => res.json({ message: 'Financer BE API - author: Jad Samadi' }));
 
     DI.server = app.listen(4000, "localhost", () => {
-        console.log(`listening on 4000`)
+        console.log(`listening on http://localhost:4000`)
     });
 })();
